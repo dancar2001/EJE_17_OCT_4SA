f0fb95ba8156efd20a9dd6acab95ecf2f21250b0
Administrador <Administrador@PC0717>
2022-10-17 08:47:32 -0500

    Agregar archivos de proyecto.

---
+++ EJE_17_OCT_4SA/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJE_17_OCT_4SA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
